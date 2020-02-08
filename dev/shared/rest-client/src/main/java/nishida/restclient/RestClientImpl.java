d6eaf9c8ec55686357c985579cad46698bb17184
Tyler Nishida <tnishida95@gmail.com>
2020-02-08 11:18:06 -0800

    filter on name column only

--- dev/shared/rest-client/src/main/java/nishida/restclient/RestClientImpl.java
+++ dev/shared/rest-client/src/main/java/nishida/restclient/RestClientImpl.java
@@ -15,24 +15,24 @@ import org.apache.http.ssl.SSLContexts;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-import java.security.KeyStore;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyStore;
+import java.util.Optional;
 
 @Component("restTemplateImpl")
 public class RestClientImpl implements RestClient {
 
-	@Value("${certificate.path}")
-	private String certificatePath;
+	@Value("${certificate.path:#{null}}")
+	private Optional<String> certificatePath;
 
-	@Value("${certificate.password}")
-	private String certificatePassword;
+	@Value("${certificate.password:#{null}}")
+	private Optional<String> certificatePassword;
 
 	private RestTemplate restTemplate;
 
 	@PostConstruct
 	private void init() throws Exception {
-
 		restTemplate = new RestTemplate(getClientHttpRequestFactory());
 	}
 
@@ -41,9 +41,12 @@ public class RestClientImpl implements RestClient {
 	}
 
 	private HttpClient getHttpClient() throws Exception {
+		if(!certificatePath.isPresent() || !certificatePassword.isPresent()) {
+			return HttpClients.createDefault();
+		}
 		KeyStore ks = KeyStore.getInstance("pkcs12");
-		char[] password = certificatePassword.toCharArray();
-		try (InputStream keyStoreStream = new ClassPathResource(certificatePath, this.getClass().getClassLoader()).getInputStream()) {
+		char[] password = certificatePassword.get().toCharArray();
+		try (InputStream keyStoreStream = new ClassPathResource(certificatePath.get(), this.getClass().getClassLoader()).getInputStream()) {
 			ks.load(keyStoreStream, password);
 			SSLContext sslContext = SSLContexts.custom().loadKeyMaterial(ks, password).build();
 			return HttpClients.custom().setSSLContext(sslContext).build();
